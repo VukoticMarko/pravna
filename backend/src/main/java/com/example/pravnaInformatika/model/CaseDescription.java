3d841be051ba3e711f4558de7471fab5dd523b76
VukoticMarko <50705538+VukoticMarko@users.noreply.github.com>
2024-03-12 15:01:29 +0100

    Added 412 and 413 laws

--- backend/src/main/java/com/example/pravnaInformatika/model/CaseDescription.java
+++ backend/src/main/java/com/example/pravnaInformatika/model/CaseDescription.java
@@ -4,15 +4,23 @@ public class CaseDescription {
 	
 	
 	private int id;
-    private String court;
+    private String court; // Sud
     private String businessNumber; // poslovniBroj, brojSlucaja
     private String judge; // Sudija
     private String defendant; // Okrivljeni
     private String felony; // Krivicno delo
+    
+    // 412
     private boolean fakeDocument; // Napravljen neistinit dokument
     private boolean publicDocument; // Napravljen neistinit javni dokument
     private boolean triedMaking; // Pokusaj pravljenja neistinitog dokumenta
     
+    // 413
+    private String signatureWithoutPermission; // Neovlasceno popunjene izjave koja imaju vrednost za pravne odnose
+    private String fraudulentDocument; // Obmane sadrzaja dokumenta
+    private String documentWithoutPermission; // Isprava izdata u ime drugog lica bez njegove saglasnosti
+    private String fraudulentRank; // Ako se u svom potpisu stavi cin/rank/polozaj i ako nema taj cin/rank/polozaj
+    private String sealWithoutPermission; // Koriscenje pravog pecata/znaka bez dozvole fizickog, pravnog (lica) ili neke institucije
     
     private String recorder; // Zapisnicar
     private String officialPerson; // Sluzbeno lice
