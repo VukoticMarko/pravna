5de1a21b8662201c2b4a05b90a4a3530beb33f58
VukoticMarko <50705538+VukoticMarko@users.noreply.github.com>
2024-03-12 14:35:40 +0100

    Model v1

---
+++ backend/src/main/java/com/example/pravnaInformatika/model/CaseDescription.java
@@ -0,0 +1,21 @@
+package com.example.pravnaInformatika.model;
+
+public class CaseDescription {
+	
+	
+	private int id;
+    private String court;
+    private String businessNumber; // poslovniBroj, brojSlucaja
+    private String judge; // Sudija
+    private String defendant; // Okrivljeni
+    private String felony; // Krivicno delo
+    private boolean fakeDocument; // Napravljen neistinit dokument
+    private boolean publicDocument; // Napravljen neistinit javni dokument
+    private boolean triedMaking; // Pokusaj pravljenja neistinitog dokumenta
+    
+    
+    private String recorder; // Zapisnicar
+    private String officialPerson; // Sluzbeno lice
+    private String punishment; // Kazna
+
+}
