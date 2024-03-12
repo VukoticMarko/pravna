465c9beb8bc860a08a5ff602510848244254ce5a
VukoticMarko <50705538+VukoticMarko@users.noreply.github.com>
2024-03-12 13:14:36 +0100

    Initialized spring boot

---
+++ backend/src/test/java/com/example/pravnaInformatika/PravnaInformatikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pravnaInformatika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PravnaInformatikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
